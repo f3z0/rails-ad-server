2afec86d20c01330624e3c6ea3d8a46813ca598f
Gregory Ray <gregory.james.ray@gmail.com>
2008-07-07 18:01:17 -0600

    iii

--- lib/rads/controller_helpers/controller_helpers.rb
+++ lib/rads/controller_helpers/controller_helpers.rb
@@ -1,10 +1,12 @@
 module Rads
-  module ControllerHelperss
-    def rads_url(banner_id)
-      banner = RadsBanner.find(banner_id)
-      banner.rads_banner_stats[0].clicks += 1
-      banner.rads_banner_stats[0].save
-      return banner.url
+  module ControllerHelpers
+    module ClassMethods
+      def rads_url(banner_id)
+        banner = RadsBanner.find(banner_id)
+        banner.rads_banner_stats[0].clicks += 1
+        banner.rads_banner_stats[0].save
+        return banner.url
+      end
     end
   end
 end
