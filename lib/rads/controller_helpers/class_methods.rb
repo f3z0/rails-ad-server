79b7f3d2e7ca4aaf10e5e235e08a2a99fce4dcce
Gregory Ray <gregory.james.ray@gmail.com>
2008-07-07 18:11:44 -0600

    urrtr

---
+++ lib/rads/controller_helpers/class_methods.rb
@@ -0,0 +1,12 @@
+module Rads
+  module ControllerHelpers
+    module ClassMethods
+      def rads_url(banner_id)
+        banner = RadsBanner.find(banner_id)
+        banner.rads_banner_stats[0].clicks += 1
+        banner.rads_banner_stats[0].save
+        return banner.url
+      end
+    end
+  end
+end
